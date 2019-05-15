249caac6357d70b0cfcd4fb22fbfccea6c28a1ab
Gaston Rivata <gastonrivata@gmail.com>
2019-05-15 00:15:41 -0300

    Refactored some functions in usuario controller to handle better the response and catch errors. Add datos usuario handler

--- routes.js
+++ routes.js
@@ -3,8 +3,9 @@ var UsuarioController = require('./controllers/usuario-controller');
 
 var api = express.Router();
 
-api.get('/nombre/:usuario', UsuarioController.getUsuarioByNombre);
+api.post('/login', UsuarioController.getUsuarioByUsuario);
 api.get('/id/:id', UsuarioController.getUsuarioById);
 api.post('/sign-up', UsuarioController.setUsuarioPassword);
+api.post('/datos-personales', UsuarioController.setDatosUsuarios);
 
 module.exports = api;
